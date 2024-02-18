24c02cdf4d6f6ecb74eb7975e94b7dac82adb3ed
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2024-02-18 07:43:12 -0600

    types

---
+++ basics/types.ts
@@ -0,0 +1,18 @@
+let userName: string = "Juan";
+let age: number = 32;
+let enables: boolean = true;
+
+let person: Object = {};
+let fruits: Object[] =[{}, {name: "manzana"}];
+
+let response: any = "Hola"
+
+function sendNotes(): void {
+    console.log("Message from TS");
+}
+
+let responseUnknowm: unknown;
+responseUnknowm = true;
+
+let responseNull = null;
+let responseUndedined = undefined;
---
+++ dist/types.js
@@ -0,0 +1,13 @@
+var userName = "Juan";
+var age = 32;
+var enables = true;
+var person = {};
+var fruits = [{}, { name: "manzana" }];
+var response = "Hola";
+function sendNotes() {
+    console.log("Message from TS");
+}
+var responseUnknowm;
+responseUnknowm = true;
+var responseNull = null;
+var responseUndedined = undefined;
